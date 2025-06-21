c863547774e8ea8682a1495f50fbf03a149e234e
Modular-Category <modular.tensor.category@gmail.com>
2025-06-21 12:49:43 +0900

    フォルダ名の修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/tutorials/basic-elements">
+            to="/docs/tutorials/basic_elements">
             5分で使い始める
           </Link>
         </div>
