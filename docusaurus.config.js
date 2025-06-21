4bf9ded418e46386ada0adde47597330102ab848
Modular-Category <modular.tensor.category@gmail.com>
2025-06-21 12:20:29 +0900

    docu chan oide

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   tagline: 'モノイダル圏のための直感的なLaTeX図式計算パッケージ',
   // highlight-start
   url: 'https://Modular-Category.github.io', // ★ あなたのGitHubユーザー名に置き換える
-  baseUrl: '/qworld-docs/', // ★ あなたのリポジトリ名に置き換える
+  baseUrl: '/blog-demo4/', // ★ あなたのリポジトリ名に置き換える
   // highlight-end
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // highlight-start
   organizationName: 'Modular-Category', // ★ あなたのGitHubユーザー名
-  projectName: 'qworld-docs', // ★ あなたのリポジトリ名
+  projectName: 'blog-demo4', // ★ あなたのリポジトリ名
   // highlight-end
 
   i18n: {
@@ -60,3 +60,9 @@ const config = {
 };
 
 export default config;
+
+module.exports = {
+  // ...
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+}
