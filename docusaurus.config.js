5e7310401fd309deaa6f873ac689d775739822bb
Modular-Category <modular.tensor.category@gmail.com>
2025-06-28 01:54:01 +0900

    直接 path.join でパス参照

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
         // ... themeConfig の中身は変更なし ...
     }),
   plugins: [
-    require(path.resolve(__dirname, './plugins/docusaurus-qworld-plugin/index.js')),
+    path.join(__dirname, 'plugins/docusaurus-qworld-plugin'),
   ],
 };
 
