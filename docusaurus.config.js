7ed32c967239ad5433010b322f233b3e102bc877
Modular-Category <modular.tensor.category@gmail.com>
2025-06-28 06:24:33 +0900

    数式問題 3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,7 +49,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
             'https://github.com/Modular-Category/blog-demo4/tree/main/',
-          remarkPlugins: [[require('remark-math'), { katex: require('katex') }]],
+          rehypePlugins: [[require('rehype-katex'), {strict: false}]],
         },
         blog: {
           showReadingTime: true,
